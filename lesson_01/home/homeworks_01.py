44de4f2eff3eae86cc114ac2b08074721b7e8b6d
Tetiana Linichenko <linichenko3@gmail.com>
2026-08-05 16:34:11 +0300

    Домашнє завдання 1 виконала

--- lesson_01/home/homeworks_01.py
+++ lesson_01/home/homeworks_01.py
@@ -1,28 +1,28 @@
 # task 01 == Виправте синтаксичні помилки
 print("Hello", end = " ")
-    print("world!")
+print("world!")
 
 # task 02  == Виправте назви змінних, щоб текст виводався
 hello = "Hello"
 world = "world"
-print(f"{helo} {word}!")
+print("{helo, word}!")
 
 # task 03 == Зробіть так, щоб кількість бананів була
 # завжди на чотири штуки більша, ніж яблук
 apples = 2
-banana = # код тут
+banana = apples + 4
 
 
 # task 04 == виправте назви змінних
-1_storona = 1
-?torona_2 = 2
-сторона_3 = 3
-$torona_4 = 4
+storona_1 = 1
+storona_2 = 2
+storona_3 = 3
+storona_4 = 4
 
 # task 05 == Порахуйте периметр фігури з task 04
 # та виведіть його для користувача
-perimetery = ? + ? + ? + ?
-print()
+perimetery = storona_1 + storona_2 + storona_3 + storona_4
+print(perimetery)
 
 
 """
@@ -40,12 +40,24 @@ print()
 1 марку із серії «Звірі» вона подарувала подружці. 
 Скільки марок лишилось у Оксани?
 """
+# task 06
+mark_mistectvo=20
+mark_zviri=7
+mark_podarynok_1=mark_mistectvo-5
+mark_podarynok_2=mark_zviri-1
+vsogo=mark_podarynok_1+mark_podarynok_2
+print (vsogo)
 
 # task 07
 """
 У саду посадили 4 яблуні. Груш на 5 більше яблунь, а слив - на 2 менше.
 Скільки всього дерев посадили в саду?
 """
+yabluna = 4
+grusha = yabluna + 5
+sliva = yabluna - 2
+vsogo = yabluna + grusha + sliva
+print (vsogo)
 
 # task 08
 """
@@ -60,11 +72,26 @@ print()
 1 хлопчик захворів та 2 дівчинки не прийшли сьогодні.
 Скількі сьогодні дітей у театральному гуртку?
 """
+hlopchik = 24
+divchatka = 24//2
+hlopchsk_priyshov = hlopchik - 1
+divchatka_priyshov = divchatka - 2
+vsogo = hlopchsk_priyshov + divchatka_priyshov
+print (vsogo)
 
 # task 10
 """
 Перша книжка коштує 8 грн., друга - на 2 грн. дороже,
 а третя - як половина вартості першої та другої разом.
 Скільки будуть коштувати усі книги, якщо купити по одному примірнику?
+
 """
+kniga_1 = 8
+kniga_2 = kniga_1 + 2
+kniga_3 = (kniga_1 + kniga_2)/2
+vsogo = kniga_1 + kniga_2 + kniga_3
+print (vsogo)
+
+
+
 
